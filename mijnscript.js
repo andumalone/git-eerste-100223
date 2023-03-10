7f6db0b2e91c406f0e4d0d0d39a00db1644eef0a
Andu <andumalone@gmail.com>
2023-03-10 13:56:09 +0100

    mijn eerste

---
+++ mijnscript.js
@@ -0,0 +1,38 @@
+    function hierTeBeginnen(){
+        //alert("hier iets laten zien")
+        console.log("iets anders laten zienn");
+        var mijnvar1 =24;
+        console.log(mijnvar1 + 44);
+       console.log(mijnvar1==24);
+       var mijnvar2 = 55;
+       console.log(mijnvar1 ==24 || mijnvar2  == 53);
+       console.log(mijnvar1 != mijnvar2);
+       console.log(mijnvar1 < mijnvar2);
+       console.log(mijnvar1 => mijnvar2);
+       console.log(mijnvar1 ==24 && mijnvar2  == 53);
+       console.log(10 % 3);
+       console.log(10 ** 3);
+      var mijnvar3 = 12;
+      mijnvar3++;
+      mijnvar3 += 14;
+      console.log(mijnvar3);
+      mijnvar2 == 5 ? console.log("go"):console.log("no");
+
+        //arithmic operators
+        //+-*/ % **
+
+        //assignment operator
+        // = -- ++ += -=
+
+        //comparative operator refereert altijd naar boolean
+        // ==(kijk naar waarde) ===(kijkt naar type) !=(word true als iets ongelijk is)
+        // <> <= >=
+
+        // logische operator >> operator moeten bool zijn
+        // ||(pipe) of als beide kanten ture zijn
+        // &&  allebei waar
+        // ^ xor alleen als een van de twee waar is true
+        // ! flipt het resultaat
+
+
+    }
